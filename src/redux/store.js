2500f71cd704d2e29322fad959a75c294eb22ea8
Diego Felipe Triviño <diegotri.89@gmail.com>
2020-06-16 21:28:53 -0500

    test Middleware logger (create for me)

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,10 +7,16 @@ const rootReducer = combineReducers({
     pokemons: pokeReducer
 })
 
+const logger = store => next => action => {
+    console.log('dispatching', action)
+    let result = next(action)
+    console.log('next state', store.getState())
+    return result
+  }
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function generateStore(){
-    const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+    const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk,logger)))
     return store
 }
